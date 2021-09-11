70021a9ad0d95c04bd40028491c585cc02c9d50b
kim-hyunjin <hyunjin1612@gmail.com>
2021-09-11 22:13:42 +0900

    express, view setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.join(path.resolve(), "/src");
 
 const app = express();
 
-console.log("server start...");
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(3000, () => {
+  console.log("server start...");
+});
