8715f0d34cf739a873064cd588cf23705c57e90d
Kim <hyunjin1612@gmail.com>
2021-09-26 20:52:18 +0900

    socket.io 이벤트 처리

--- socketio/src/public/js/app.js
+++ socketio/src/public/js/app.js
@@ -1,1 +1,20 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function backendDone(msg) {
+  console.log(`backend says: ${msg}`);
+}
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  // 첫번째는 이벤트명을 넣어준다.
+  // 두번째 인자부터 다양한 타입의 데이터를 가변인자로 제한없이 보낼 수 있다.
+  // 마지막 인자로 서버작업완료시 호출받을 콜백함수를 넣는다.
+  socket.emit("enter_room", input.value, backendDone);
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
--- socketio/src/server.js
+++ socketio/src/server.js
@@ -17,7 +17,12 @@ const httpServer = http.createServer(app);
 const wsServer = new SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
+    setTimeout(() => {
+      done("hello from the backend!");
+    }, 10000);
+  });
 });
 
 httpServer.listen(3000, () => {
