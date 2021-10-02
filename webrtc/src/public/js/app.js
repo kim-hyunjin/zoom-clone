d7c7cb3f3fa48f41e1b6577b0839c3e48556b331
kim-hyunjin <hyunjin1612@gmail.com>
2021-10-02 17:48:07 +0900

    WebRTC로 peer to peer video call 구현 완료

--- webrtc/src/public/js/app.js
+++ webrtc/src/public/js/app.js
@@ -4,12 +4,21 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const cameraSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getMedia(deviceId) {
+  const initConfig = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
   try {
     myStream = await navigator.mediaDevices.getUserMedia(
       deviceId
@@ -17,10 +26,7 @@ async function getMedia(deviceId) {
             audio: true,
             video: { deviceId: { exact: deviceId } },
           }
-        : {
-            audio: true,
-            video: { facingMode: "user" },
-          }
+        : initConfig
     );
     myFace.srcObject = myStream;
     if (!deviceId) {
@@ -37,6 +43,7 @@ async function getCameraSelectOptions() {
     const cameras = devices.filter((device) => device.kind === "videoinput");
     const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
+      console.log("camera", camera);
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
@@ -76,8 +83,95 @@ cameraBtn.addEventListener("click", () => {
   }
 });
 
-cameraSelect.addEventListener("input", async () => {
+cameraSelect.addEventListener("input", handleCameraChange);
+
+async function handleCameraChange() {
   await getMedia(cameraSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    // 다른 브라우저로 보내진 비디오와 오디오 데이터를 컨트롤 하기 위해 Sender를 사용.
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
+}
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+welcomeForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName = input.value;
+  input.value = "";
 });
 
-getMedia();
+// 다른 브라우저가 접속하면 연결하기 위해 offer를 만들어 보내야 한다.
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+});
+
+// 다른 브라우저로부터 offer가 오면 answer를 만들어 보낸다.
+socket.on("offer", async (offer) => {
+  console.log("receive offer", offer);
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  console.log("sent the answer");
+  socket.emit("answer", answer, roomName);
+});
+
+// 다른 브라우저로부터 answer가 오면 설정한다.
+socket.on("answer", (answer) => {
+  console.log("receive answer", answer);
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  console.log("received cadidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+
+// RTC
+function makeConnection() {
+  // STUN(Session Traversal Utilities for NAT)서버를 사용해 핸드폰과 컴퓨터가 서로의 공용 IP를 찾을 수 있도록 설정하기
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ], // 구글에서 제공하는 STUN서버 사용
+  });
+  myPeerConnection.addEventListener("icecandidate", (data) => {
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+  });
+  myPeerConnection.addEventListener("addstream", (data) => {
+    console.log("Peer's Stream", data.stream);
+    console.log("My Stream", myStream);
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+  });
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
